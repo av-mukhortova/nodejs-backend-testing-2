c43185ae280f87e1351a83ed8752260af6f1517a
av-mukhortova <av.mukhortova@gmail.com>
2025-03-31 12:18:11 +0300

    feat: add tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -14,6 +14,8 @@ describe('PostsService', () => {
       {text: 'Post 3'},
       {text: 'Post 4'},
     ];
+    const skip = 1;
+    const limit = 2;
 
     beforeEach(() => {
       posts.forEach((post) => postsService.create(post));
@@ -21,12 +23,29 @@ describe('PostsService', () => {
 
     it('should return all posts if called without options', () => {
       // реализуйте тест-кейс
+      expect(postsService.findMany().length).toEqual(posts.length);
     });
 
     it('should return correct posts for skip and limit options', () => {
       // реализуйте тест-кейс
+      const res = postsService.findMany({ skip, limit}).map((item) => item.text);
+      const expectedRes = posts.slice(skip,limit + 1).map((item) => item.text);
+      expect(res).toEqual(expectedRes);
     });
 
     // реализуйте недостающие тест-кейсы
+    it('should return correct posts for skip option only', () => {
+      // реализуйте тест-кейс
+      const res = postsService.findMany({ skip}).map((item) => item.text);
+      const expectedRes = posts.slice(skip).map((item) => item.text);
+      expect(res).toEqual(expectedRes);
+    });
+
+    it('should return correct posts for limit option only', () => {
+      // реализуйте тест-кейс
+      const res = postsService.findMany({ limit}).map((item) => item.text);
+      const expectedRes = posts.slice(0, limit).map((item) => item.text);
+      expect(res).toEqual(expectedRes);
+    });
   });
 });
